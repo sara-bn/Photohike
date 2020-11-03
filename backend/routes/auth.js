1bf9e162496444d2ef6ce73ae74e0903248c1687
Sara <sara_banaeirad@hotmail.com>
2020-11-03 01:37:59 -0800

    login and middleware added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,54 @@
 //login
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+// @route    POST api/auth
+// @desc     Authentication user $ get token
+// @access   Public
+router.post(
+	'/',
+	[check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()],
+	async (req, res) => {
+        const { email, password } = req.body;
+
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.status(400).json({ errors: [{ msg: 'Please register first' }] });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '5 days' }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
+
+module.exports = router;
