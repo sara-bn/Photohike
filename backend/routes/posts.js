dd7ab611bd34f50cc710e810d6ea83327db23229
Sara <sara_banaeirad@hotmail.com>
2020-11-04 00:58:44 -0800

    Getall + Get +Delete posts added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -9,7 +9,6 @@ const Post = require('../models/Post');
 // @desc     add post
 // @access   Private
 router.post('/', [auth, [check('caption', 'Caption is required').not().isEmpty()]],  async (req, res) => {
-    console.log(req);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -32,5 +31,61 @@ router.post('/', [auth, [check('caption', 'Caption is required').not().isEmpty()
     }
     }
 );
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Public
+router.get('/', async (req, res) => {
+    console.log(req.user);
+	try {
+		//sort posts by data
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route    GetByUserId api/posts/:id
+// @desc     Get all posts from a user
+// @access   Private
+router.get('/myposts', auth, async (req, res) => {
+	try {
+			const posts = await Post.find({userId:req.user.id});
+            res.json(posts);
+            
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete('/:id', auth, async (req, res) => {
+	try {
+			const post = await Post.findById(req.params.id);
+
+			if (!post) {
+				return res.status(404).json({ msg: 'Post not found' });
+			}
+
+			// Check user
+			if (post.user.toString() !== req.user.id) {
+				return res.status(401).json({ msg: 'User not authorized' });
+			}
+
+			await post.remove();
+
+			res.json({ msg: 'Post removed' });
+	} catch (err) {
+		console.error(err.message);
+
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
